205b6e544032ff054d8c79236147b27e38c8773c
Sen <tug81999@temple.edu>
2021-09-20 02:53:40 -0400

    pruned some unused imports

--- app/src/main/java/edu/temple/convoy/MainActivity.java
+++ app/src/main/java/edu/temple/convoy/MainActivity.java
@@ -6,8 +6,6 @@ import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.util.Log;
 import android.widget.EditText;
 import android.widget.TextView;
 
